9c4f0d996d730e023363ca95259f702dd7880a08
linus.schulze <linuscyber146@gmail.com>
2026-02-11 00:30:54 +0100

    Markdown struct tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -39,3 +39,12 @@ fn test_create_markdown_object() {
         html: "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Hello</title>\n</head>\n<body>\n    <h1>Hi</h1>\n</body>\n</html>".to_string(),
     })
 }
+
+#[test]
+#[should_panic]
+fn test_create_markdown_object_wrong_filepath() {
+    let config = Config::build(vec![String::from("hey"), String::from("index.html"), String::from("hello2.md")]
+        .into_iter()).unwrap();
+
+    let _mk_file = read_md_file(&config).expect("something is wrong with the md file");
+}
